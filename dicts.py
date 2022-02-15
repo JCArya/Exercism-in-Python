d8b2e54bb65394a374d4a679725ef6034bf84fc3
Jagdish Arya <98749933+JCArya@users.noreply.github.com>
2022-02-15 14:09:03 +0530

    Add files via upload

---
+++ dicts.py
@@ -0,0 +1,60 @@
+def create_inventory(items):
+    """
+    :param items: list - list of items to create an inventory from.
+    :return:  dict - the inventory dictionary.
+    """
+
+    inventory = {}
+    add_items(inventory, items)
+    return inventory
+
+
+def add_items(inventory, items):
+    """
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return:  dict - the inventory dictionary update with the new items.
+    """
+
+    for item in items:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+
+def decrement_items(inventory, items):
+    """
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return:  dict - updated inventory dictionary with items decremented.
+    """
+
+    for item in items:
+        if item in inventory:
+            inventory[item] = max(inventory[item] - 1, 0)
+    return inventory
+
+
+def remove_item(inventory, item):
+    """
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return:  dict - updated inventory dictionary with item removed.
+    """
+
+    if item in inventory:
+        inventory.pop(item)
+    return inventory
+
+
+def list_inventory(inventory):
+    """
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+
+    output = []
+    for item in sorted(inventory.items()):
+        if item[1] > 0:
+            output.append(item)
+    return output
