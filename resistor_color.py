dd2c53ee52671fbadcdba2149f31cc125aef0c2f
Jagdish Arya <98749933+JCArya@users.noreply.github.com>
2022-02-15 14:16:27 +0530

    Add files via upload

---
+++ resistor_color.py
@@ -0,0 +1,15 @@
+def color_code(color):
+    return colors().index(color)
+def colors():
+    return [
+        'black',
+        'brown',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'violet',
+        'grey',
+        'white'
+    ]
