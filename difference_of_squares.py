9bf2bb9b6335b5e050542275f1a196a60c5d789e
Jagdish Arya <98749933+JCArya@users.noreply.github.com>
2022-02-27 03:22:10 -0800

    Add files via upload

---
+++ difference_of_squares.py
@@ -0,0 +1,16 @@
+def square_of_sum(number):
+
+    if number == 1:
+        
+        return 1
+        
+    else:
+        return (number * (number + 1) / 2) ** 2
+
+def sum_of_squares(number):
+
+    return  number * (number + 1) * (2 * number + 1) / 6
+    
+def difference_of_squares(number):
+    
+    return square_of_sum(number) - sum_of_squares(number)
