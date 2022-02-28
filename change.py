6c88ff5d8784c6d1563c7b8c2f404b245f92ee0a
Jagdish Arya <98749933+JCArya@users.noreply.github.com>
2022-02-28 14:08:25 +0530

    Add files via upload

---
+++ change.py
@@ -0,0 +1,13 @@
+
+from itertools import combinations_with_replacement
+def find_fewest_coins(coins, target):
+    if target < 0:
+        raise ValueError("target can't be negative")
+    max_coins = 1 + target // min(coins)
+    
+    for i in range(max_coins):
+        combos = combinations_with_replacement(coins, i)
+        for combo in combos:
+            if sum(combo) == target:
+                return sorted(combo)
+    raise ValueError("can't make target with given coins")
